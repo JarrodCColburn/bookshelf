0cbda5ff3c454d0bab3df76b3d803a49ae23c6cd
Jarrod Colburn <jarrodcolburn@jcc-mbp.attlocal.net>
2018-01-02 23:51:59 -0600

    feat: setup book to use API data

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,16 @@
 import React from "react";
 class Book extends React.Component {
   render() {
+    let books
+    if ( this.props.books ) {
+      ({ books } = this.props )
+    }
+    else {
+      return (<div />)
+    }
+    let { authors, title, imageLinks } = books
+    let author = Array.isArray(authors) && authors.length && authors[0] || ''
+    let image = imageLinks && ( imageLinks.thumbnail || imageLinks.smallThumbnail ) || '';
     return (
       <li>
       <div className="book">
@@ -10,8 +20,9 @@ class Book extends React.Component {
             style={{
               width: 128,
               height: 193,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+              'backgroundImage':
+                `url($(image))`
+                // 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
             }}
           />
           <div className="book-shelf-changer">
@@ -26,8 +37,8 @@ class Book extends React.Component {
             </select>
           </div>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{author}</div>
       </div>
       </li>
     );

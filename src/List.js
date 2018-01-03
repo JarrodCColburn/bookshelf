72c882e69eedcf40bea9fba2f44d1dc916014087
Jarrod Colburn <jarrodcolburn@jcc-mbp.attlocal.net>
2018-01-03 00:34:47 -0600

    feat: books filtered by shelves

--- src/List.js
+++ src/List.js
@@ -1,35 +1,37 @@
-import React from 'react'
-import Book from './Book'
+import React from "react";
+import Book from "./Book";
 var { books } = require("./books.json");
-var shelfs = ['read','wantToRead','currentlyReading']
+var shelfs = ["read", "wantToRead", "currentlyReading"];
 class List extends React.Component {
-  render(){
+  render() {
     return (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-      {shelfs.map( shelf => (
-      
-                <div className="bookshelf" key={shelf}>
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">{books.map( book => (<Book key={book.id} book={book} />)
-                    ) }</ol>
-                  </div>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {shelfs.map(shelf => (
+              <div className="bookshelf" key={shelf}>
+                <h2 className="bookshelf-title">{shelf}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {books
+                      .filter(book => book.shelf === shelf)
+                      .map(book => <Book key={book.id} book={book} />)}
+                  </ol>
                 </div>
-      )
-      
-      )}
               </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
+            ))}
           </div>
-        )
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>
+            Add a book
+          </a>
+        </div>
+      </div>
+    );
   }
 }
-export default List
+export default List;

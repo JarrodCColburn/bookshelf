11b6c525c7ec7942f6aed5cdf0ee57e6166a426a
Jarrod Colburn <jarrodcolburn@jcc-mbp.attlocal.net>
2018-01-03 20:30:07 -0600

    docs: added propTypes to Book

--- src/Book.js
+++ src/Book.js
@@ -1,34 +1,37 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 class Book extends React.Component {
+  static propTypes = {
+    value: PropTypes.string,
+    shelfs: PropTypes.array,
+    changeShelf: PropTypes.func,
+  };
   render() {
-    let book
-    // debugger
-    if ( this.props.book ) {
-      ({book} = this.props )
-    }
-    else {
-      return (<li><div>Something Here</div></li>)
-    }
-    let { authors, title, imageLinks } = book
-    let author = (Array.isArray(authors) && authors.length && authors[0] )|| ''
-    let image = (imageLinks && ( imageLinks.thumbnail || imageLinks.smallThumbnail )) || '';
+    let book;
+    if (this.props.book) {
+      ({book} = this.props);
+    }  
+    let {authors, title, imageLinks} = book;
+    let author = (Array.isArray(authors) && authors.length && authors[0]) || '';
+    let image =
+      (imageLinks && (imageLinks.thumbnail || imageLinks.smallThumbnail)) || '';
     return (
       <li>
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              'backgroundImage': `url(${image})`
-            }}
-          />
-      {this.props.children}
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${image})`,
+              }}
+            />
+            {this.props.children}
+          </div>
+          <div className="book-title">{title}</div>
+          <div className="book-authors">{author}</div>
         </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">{author}</div>
-      </div>
       </li>
     );
   }

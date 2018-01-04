50de4defb08e7dc39a3c38e1afdd44ef3a0b1c1b
Jarrod Colburn <jarrodcolburn@jcc-mbp.attlocal.net>
2018-01-03 21:27:54 -0600

    finished

--- src/Changer.js
+++ src/Changer.js
@@ -1,11 +1,13 @@
 import toTitleCase from 'to-title-case';
 import PropTypes from 'prop-types';
 import React from 'react';
-/** 
+/**
  * @description Allows for options to select shelf
  *
  */
 class Changer extends React.Component {
+  // Not including state at this time because
+  // component is automatically unmounted upon change
   static propTypes = {
     value: PropTypes.string,
     shelfs: PropTypes.array,
@@ -19,15 +21,15 @@ class Changer extends React.Component {
     let shelfs = this.props.shelfs || [];
     let value = this.props.value || 'none';
     return (
-      <div className='book-shelf-changer'>
+      <div className="book-shelf-changer">
         <select onChange={this.handleChange} value={value}>
-          <optgroup label='Move to...'>
+          <optgroup label="Move to...">
             {shelfs.map(shelf => (
               <option key={shelf} value={shelf}>
                 {toTitleCase(shelf)}
               </option>
             ))}
-            <option value='none'>None</option>
+            <option value="none">None</option>
           </optgroup>
         </select>
       </div>
